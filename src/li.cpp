9f121f703084258e632aeaef94f8c9b803892f8b
Yohann <yohanndu77@hotmail.fr>
2018-09-26 09:45:56 +0200

    ajout des fonctions de calcul

--- src/li.cpp
+++ src/li.cpp
@@ -5,3 +5,13 @@ int calcul::somme(int nb1, int nb2){
 return (nb1+nb2);
 
 }
+int calcul::multiplication(int nb1, int nb2){
+return (nb1*nb2);
+}
+int calcul::division(int nb1, int nb2){
+return (nb1/nb2);
+}
+
+int calcul::soustraction(int nb1, int nb2){
+return (nb1-nb2);
+}
--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,18 @@
 #include "li.h"
 #include "ecole.h"
 //#include <stdlib.h>
+using namespace std;
 
 int main() {
 
-//std::cout << "Hello world" << std::endl;
-//calcul a;
-//std::cout << a.somme(5,10) << std::endl;
-ecole date;
-std::cout << "L'année de création est " << date.annee(1950) << std::endl;
-return 0; 
+	calcul a;
+
+	cout << "somme: " << a.somme(5,10) << endl;
+	cout << "multiplication: " << a.multiplication(10,10) << std::endl;
+	cout << "division: " << a.division(10,2) << endl;
+	cout << "soustraction: " << a.soustraction(50,15) << endl;
+	//ecole date;
+	//std::cout << "L'année de création est " << date.annee(1950) << std::endl;
+	return 0; 
 
 }
